e334ae20aa9f41d48bd742e160a31fdee38c9bfe
Tobiasz Ciesielski <46262617+tobikodzi@users.noreply.github.com>
2019-12-01 19:54:46 +0100

    day 1

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-def fuel(n):
-    n = int(n / 3) - 2
-    if n > 0:
-        return n + (fuel(n))
-    else:
-        return 0
-
-
 def day_1():
+    def fuel(n):
+        n = int(n / 3) - 2
+        if n > 0:
+            return n + (fuel(n))
+        else:
+            return 0
+
     content = open('inputs/input_day1.txt', mode='r').readlines()
     print('part 1:', sum([int(float(n) / 3) - 2 for n in content]))
     print('part 2:', sum([fuel(int(n)) for n in content]))
