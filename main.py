629cbfe7de362116c425d7070117f6a6e436325b
Tobiasz Ciesielski <46262617+tobikodzi@users.noreply.github.com>
2019-12-04 00:06:29 +0100

    day_2

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+
+def day_2():
+    content = open('inputs/input_day2.txt', mode='r').read()
+    program = [int(x) for x in content.split(sep=',')]
+    program[1] = 12
+    program[2] = 2
+    i = 0
+    while program[i] != 99:
+        a = program[i + 1]
+        b = program[i + 2]
+        c = program[i + 3]
+        if program[i] == 1:
+            program[c] = program[a] + program[b]
+        elif program[i] == 2:
+            program[c] = program[a] * program[b]
+        i += 4
+    print(program[0])
+
+
 def day_1():
     def fuel(n):
         n = int(n / 3) - 2
@@ -12,4 +31,4 @@ def day_1():
 
 
 if __name__ == '__main__':
-    day_1()
+    day_2()
