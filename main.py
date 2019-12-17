954e42ca3e40a558acdb178852827af587ebf352
Tobiasz Ciesielski <46262617+tobikodzi@users.noreply.github.com>
2019-12-17 21:01:48 +0100

    day_5

--- main.py
+++ main.py
@@ -41,6 +41,42 @@ def day_8():
             print()
 
 
+def day_5():
+    def parse_code(code):
+        mode = str(code)
+        mode = '0' * (5 - len(mode)) + mode
+        modes = [int(x) for x in mode[:3]][::-1]
+        instruction = mode[-1]
+        return int(instruction), modes
+
+    program = [int(x) for x in open('inputs/input_day5.txt').read().split(sep=',')]
+    program_steps = [0, 3, 3, 1, 1, 2, 2, 3, 3]
+
+    i = 0
+    while program[i] != 99:
+        opcode, modes = parse_code(program[i])
+        operands = [program[i+x+1] if modes[x] else program[program[i+x+1]] for x in range(program_steps[opcode])]
+        if opcode == 1:
+            program[program[i+3]] = operands[0] + operands[1]
+        elif opcode == 2:
+            program[program[i+3]] = operands[0] * operands[1]
+        elif opcode == 3:
+            program[program[i+1]] = int(input("input: "))
+        elif opcode == 4:
+            print(operands[0])
+        elif opcode == 5:
+            i = operands[1] - 3 if operands[0] != 0 else i
+        elif opcode == 6:
+            i = operands[1] - 3 if operands[0] == 0 else i
+        elif opcode == 7:
+            program[program[i+3]] = 1 if operands[0] < operands[1] else program[program[i+3]] = 0
+        elif opcode == 8:
+                program[program[i+3]] = 1 if operands[0] == operands[1] else program[program[i+3]] = 0
+
+        i += program_steps[opcode] + 1
+
+
+
 # part 1: 0.5545520782470703 s
 def day_4():
     r = [num for num in "153517 - 630395".split(sep=' - ')]
@@ -148,6 +184,7 @@ if __name__ == '__main__':
     # day_2()
     # day_3()
     # day_4()
+    day_5()
     # day_8()
     end = time.time()
     print("time:", end - start, "s")
